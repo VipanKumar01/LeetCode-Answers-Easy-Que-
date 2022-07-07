9fb8c19550a5e4d270734325f7fba618f2970349
Vipan Kumar <108624219+VipanKumar01@users.noreply.github.com>
2022-07-07 14:04:59 +0530

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -3,9 +3,12 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)): # 0 to length of nums-1
-            for j in range(len(nums)): # same as Above
-                if nums[i]+nums[j] == target:   
-                    return [i,j]    # If True Then Return Value and Exit
-
+        l=[]
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):     
+                if nums[j]+nums[i]== target:  
+                    l.append(i)
+                    l.append(j)
+                    break
+        return l     
 # --HappyCode--
